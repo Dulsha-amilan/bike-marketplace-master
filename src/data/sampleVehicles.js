436208b0ed93661344be569c44fe840db71ba6d8
Your Name <your.email@example.com>
2025-11-15 00:06:28 +0530

    Remove unused sample vehicle data from sampleVehicles.js to streamline data structure.

--- src/data/sampleVehicles.js
+++ src/data/sampleVehicles.js
@@ -2,30 +2,7 @@
 // Items: 9
 
 export const sampleVehicles = [
- {
-  id: 'user-sdfsdf-sdfsdf-1759665794502-p1srr',
-  type: 'scooters',
-  title: 'dsfsdfsdf',
-  make: 'sdfsdf',
-  model: 'sdfsdf',
-  year: 2234,
-  registerYear: 4324,
-  price: null,
-  mileageKm: 234234,
-  engineCc: 123,
-  engineCapacityCc: 123,
-  transmission: 'werwer',
-  fuelType: 'werwer',
-  color: 'werwe',
-  condition: 'New',
-  location: 'wewrwer',
-  postedAt: '2025-10-05T12:03:14.502Z',
-  phone: '1231231232',
-  image: 'https://delivery.contenthub.honda.com.au/api/public/content/eaa48dfb342f41e48023e5b077957f5b?v=5a32408d',
-  gallery: [],
-  categories: [],
-  tags: []
-},
+ 
 
 {
   id: 'user-honda-hornet-250-1763050794654-tsua1',
