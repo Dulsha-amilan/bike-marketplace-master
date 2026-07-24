743d30d5b24630176934ae643d2008e180a98ca3
Dulsha Senavirathna <dulsha@fonixedu.com>
2026-07-24 17:06:15 +0530

    feat: add Footer component with navigation links, contact info, and language toggle

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 // components/Footer.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 import LanguageToggle from './LanguageToggle';
 import { FaFacebook, FaYoutube } from 'react-icons/fa';
 import { FiPhone, FiMail, FiMapPin } from 'react-icons/fi';
 import bikeekaLogo from '../Images/bikeeka.com logos.png';
 import './Footer.css';
 
-const Footer = ({ language, setLanguage, translations }) => {
+const Footer = ({ language, setLanguage, translations = {} }) => {
   return (
     <footer className="footer">
       <div className="container">
@@ -14,16 +15,18 @@ const Footer = ({ language, setLanguage, translations }) => {
           {/* Logo and Description */}
           <div className="footer-section">
             <div className="footer-logo">
-              <img src={bikeekaLogo} alt="bikeeka.com logo" className="footer-logo-img" />
+              <Link to="/">
+                <img src={bikeekaLogo} alt="bikeeka.com logo" className="footer-logo-img" />
+              </Link>
             </div>
             <p className="footer-description">
-              {translations.footerDescription}
+              {translations.footerDescription || "Sri Lanka's largest online marketplace for buying and selling motorcycles and scooters."}
             </p>
 
-            {/* Social Links with real icons */}
+            {/* Social Links */}
             <div className="social-links">
               <a
-                href="https://www.facebook.com/share/1GUCohtzxN/?mibextid=wwXIfr"
+                href="https://www.facebook.com/profile.php?id=61583423744748"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="social-link facebook"
@@ -33,7 +36,7 @@ const Footer = ({ language, setLanguage, translations }) => {
                 <span>Facebook</span>
               </a>
               <a
-                href="https://www.youtube.com/@ClemPerera"
+                href="https://www.youtube.com/@BikeEka"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="social-link youtube"
@@ -45,49 +48,44 @@ const Footer = ({ language, setLanguage, translations }) => {
             </div>
           </div>
 
-          {/* Quick Links */}
+          {/* Quick Links (100% Working Routes) */}
           <div className="footer-section">
-            <h4 className="footer-title">{translations.quickLinks}</h4>
+            <h4 className="footer-title">{translations.quickLinks || "Quick Links"}</h4>
             <ul className="footer-links">
-              <li><a href="#about">{translations.aboutUs}</a></li>
-              <li><a href="#how-it-works">{translations.howItWorks}</a></li>
-              <li><a href="#sell-bike">{translations.sellBike}</a></li>
-              <li><a href="#buy-bike">{translations.buyBike}</a></li>
-              <li><a href="#financing">{translations.financing}</a></li>
-              <li><a href="#help-center">{translations.helpCenter}</a></li>
-              <li><a href="#terms">{translations.termsConditions}</a></li>
-              <li><a href="#privacy">{translations.privacyPolicy}</a></li>
-              <li><a href="#contact">{translations.contactUs}</a></li>
+              <li><Link to="/">{translations.home || "Home"}</Link></li>
+              <li><Link to="/browse/all">{translations.buyBike || "Buy a Bike"}</Link></li>
+              <li><Link to="/post-ad">{translations.sellBike || "Sell Your Bike"}</Link></li>
+              <li><Link to="/spare-parts">{translations.spareParts || "Spare Parts"}</Link></li>
+              <li><Link to="/biker-gear">{translations.bikerGear || "Biker Gear"}</Link></li>
             </ul>
           </div>
 
-          {/* Categories */}
+          {/* Categories (100% Working Category Routes) */}
           <div className="footer-section">
-            <h4 className="footer-title">{translations.categories}</h4>
+            <h4 className="footer-title">{translations.categories || "Categories"}</h4>
             <ul className="footer-links">
-              <li><a href="#motorcycles">{translations.motorcycles}</a></li>
-              <li><a href="#scooters">{translations.scooters}</a></li>
-              <li><a href="#sports-bikes">{translations.sportsBikes}</a></li>
-              <li><a href="#classic-bikes">{translations.classicBikes}</a></li>
-              <li><a href="#electric-bikes">{translations.electricBikes}</a></li>
-              <li><a href="#trail-bikes">{translations.trailBikes}</a></li>
-              <li><a href="#cruiser-bikes">{translations.cruiserBikes}</a></li>
-              <li><a href="#touring-bikes">{translations.touringBikes}</a></li>
-              <li><a href="#accessories">{translations.accessories}</a></li>
+              <li><Link to="/browse/all">All Motorcycles</Link></li>
+              <li><Link to="/browse/scooters">Scooters</Link></li>
+              <li><Link to="/browse/sport">Sport Bikes</Link></li>
+              <li><Link to="/browse/cruiser">Classic / Cruiser</Link></li>
+              <li><Link to="/browse/electric">Electric Bikes</Link></li>
+              <li><Link to="/browse/trail">Trail Bikes</Link></li>
+              <li><Link to="/browse/high-capacity">High Capacity</Link></li>
+              <li><Link to="/browse/atv-adv">ATV / ADV</Link></li>
             </ul>
           </div>
 
           {/* Language and Contact */}
           <div className="footer-section">
-            <h4 className="footer-title">{translations.language}</h4>
-            <div className="footer-language">
+            <h4 className="footer-title">{translations.language || "Language & Contact"}</h4>
+            <div className="footer-language mb-3">
               <LanguageToggle 
                 language={language} 
                 setLanguage={setLanguage} 
               />
             </div>
 
-            {/* Contact Info with real icons (no emojis) */}
+            {/* Contact Info */}
             <div className="contact-info">
               <p>
                 <FiPhone className="contact-icon" aria-hidden="true" />
@@ -108,12 +106,7 @@ const Footer = ({ language, setLanguage, translations }) => {
         {/* Footer Bottom */}
         <div className="footer-bottom">
           <div className="footer-bottom-content">
-            <p>&copy; 2025 BIKE EKA.COM. {translations.allRightsReserved}</p>
-            <div className="footer-bottom-links">
-              <a href="#terms">{translations.termsConditions}</a>
-              <span className="separator">|</span>
-              <a href="#privacy">{translations.privacyPolicy}</a>
-            </div>
+            <p>&copy; {new Date().getFullYear()} BIKE EKA.COM. {translations.allRightsReserved || "All rights reserved."}</p>
           </div>
         </div>
       </div>
@@ -121,4 +114,4 @@ const Footer = ({ language, setLanguage, translations }) => {
   );
 };
 
-export default Footer;
+export default Footer;
